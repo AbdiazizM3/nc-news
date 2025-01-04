d2183978680a5befe3a04d38b18d8b7af0355e27
AbdiazizM3 <abdiazizmohamed333555@gmail.com>
2025-01-04 18:50:15 +0000

    Improved the like system

--- src/components/VoteHandler.jsx
+++ src/components/VoteHandler.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { likeArticleVotesById, dislikeArticleVotesById } from "../api"
 import { useParams } from "react-router-dom"
+import { CurrentUserContext } from "../CurrentUser"
 
 export default function VoteHandler({votes, comment_count, date}) {
+    const {currentUser} = useContext(CurrentUserContext)
     const {article_id} = useParams()
     const [voteCount, setVoteCount] = useState(votes)
-    const [isVote, setIsVote] = useState(false)
+    const isLiked = localStorage.getItem(`${currentUser}${article_id}`)
 
     function upVote() {
-        setIsVote(true)
         setVoteCount((currVotes) => {
+            localStorage.removeItem(`${currentUser}${article_id}`)
+            localStorage.setItem(`${currentUser}${article_id}`, "liked")
             return currVotes + 1
         })
 
@@ -21,8 +24,9 @@ export default function VoteHandler({votes, comment_count, date}) {
     }
 
     function downVote() {
-        setIsVote(false)
         setVoteCount((currVotes) => {
+            localStorage.removeItem(`${currentUser}${article_id}`)
+            localStorage.setItem(`${currentUser}${article_id}`, "disliked")
             return currVotes - 1
         })
 
@@ -33,23 +37,13 @@ export default function VoteHandler({votes, comment_count, date}) {
         })
     }
 
-    if(isVote){
-        return(
-            <div className="flex justify-center space-x-8">
-                <button className="article__details --votes" onClick={downVote}>👎</button>
-                <p className="article__details --votes">Votes: {voteCount}</p>
-                <p className="article__details">comments: {comment_count}</p>
-                <p className="article__details">{date}</p>
-            </div>
-        )
-    }
-
     return(
         <div className="flex justify-center space-x-8">
-            <button className="article__details --votes" onClick={upVote}>👍</button>
-            <p className="article__details --votes">Votes: {voteCount}</p>
-            <p className="article__details">comments: {comment_count}</p>
-            <p className="article__details">{date}</p>
+            {isLiked === "liked" ? <button disabled={true} className="border-solid border-2 border-slate-900 text-slate-100 bg-red-600 px-2 rounded">+</button> : <button className="border-solid border-2 border-slate-900 text-slate-100 bg-red-600 hover:bg-red-300 px-2 rounded" onClick={upVote}>+</button>}
+            <p>Votes: {voteCount}</p>
+            {isLiked === "disliked" ? <button disabled={true} className="border-solid border-2 border-slate-900 text-slate-100 bg-indigo-600 px-2 rounded">-</button> : <button className="border-solid border-2 border-slate-900 text-slate-100 bg-indigo-600 hover:bg-indigo-300 px-2 rounded" onClick={downVote}>-</button>}
+            <p>comments: {comment_count}</p>
+            <p>{date}</p>
         </div>
     )
 }
